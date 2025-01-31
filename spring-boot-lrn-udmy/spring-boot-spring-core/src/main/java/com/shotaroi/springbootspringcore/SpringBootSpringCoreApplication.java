32de203ced9a8cf563a635a83a55c69b3c6787dc
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-31 03:02:07 +0100

    Init

---
+++ spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/SpringBootSpringCoreApplication.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springbootspringcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSpringCoreApplication.class, args);
+    }
+
+}
---
+++ spring-boot-lrn-udmy/spring-boot-spring-core/src/test/java/com/shotaroi/springbootspringcore/SpringBootSpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springbootspringcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSpringCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
