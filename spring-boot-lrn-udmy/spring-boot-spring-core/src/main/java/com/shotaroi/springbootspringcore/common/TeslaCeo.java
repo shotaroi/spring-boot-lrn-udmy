deb516699e66f65758d15bf673383133ea743ef4
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-31 17:02:28 +0100

    Update TeslaCeo.java

--- spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/common/TeslaCeo.java
+++ spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/common/TeslaCeo.java
@@ -1,7 +1,9 @@
 package com.shotaroi.springbootspringcore.common;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class TeslaCeo implements Ceo {
     @Override
     public String getDailyReport() {
