791d9dd9f269314ad1e0266eb7ece30f21deeb41
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-01 00:57:09 +0100

    Update AmazonCeo.java

--- spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/common/AmazonCeo.java
+++ spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/common/AmazonCeo.java
@@ -1,8 +1,11 @@
 package com.shotaroi.springbootspringcore.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class AmazonCeo implements Ceo {
     public AmazonCeo() {
         System.out.println("In constructor: " + getClass().getSimpleName());
