5ac68687ad944462e7e4a8393d4b1b76b84da5c2
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-31 16:59:13 +0100

    Update MetaCeo.java

--- spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/common/MetaCeo.java
+++ spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/common/MetaCeo.java
@@ -1,4 +1,8 @@
 package com.shotaroi.springbootspringcore.common;
 
-public class MetaCeo {
+public class MetaCeo implements Ceo {
+    @Override
+    public String getDailyReport() {
+        return "Daily report to Mark";
+    }
 }
