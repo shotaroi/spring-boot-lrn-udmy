045fe31de90c4209104ef54b9d7350b5e66c5c1f
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-31 02:58:42 +0100

    Update MyRestController.java

--- spring-boot-lrn-udmy/01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/shotaroi/springbootdemo/rest/MyRestController.java
+++ spring-boot-lrn-udmy/01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/shotaroi/springbootdemo/rest/MyRestController.java
@@ -1,10 +1,22 @@
 package com.shotaroi.springbootdemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
+    @Value("${ceo.name}")
+    private String ceoName;
+
+    @Value("${company.name}")
+    private String companyName;
+
+    @GetMapping("/companyinfo")
+    public String getCompanyName() {
+        return "CEO: " + ceoName + ", Company name: " + companyName;
+    }
+
     @GetMapping("/")
     public String greet() {
         return "Hello world!";
