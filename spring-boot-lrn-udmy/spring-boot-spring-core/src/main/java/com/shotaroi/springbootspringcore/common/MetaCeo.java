63de09c103614dae3bd7f88d767a9be9d572d418
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-01 00:59:45 +0100

    Update MetaCeo.java

--- spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/common/MetaCeo.java
+++ spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/common/MetaCeo.java
@@ -1,5 +1,8 @@
 package com.shotaroi.springbootspringcore.common;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MetaCeo implements Ceo {
     public MetaCeo() {
         System.out.println("In constructor: " + getClass().getSimpleName());
