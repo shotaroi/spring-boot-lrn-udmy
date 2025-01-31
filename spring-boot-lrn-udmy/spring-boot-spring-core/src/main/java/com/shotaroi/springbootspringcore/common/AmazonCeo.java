743396ca0323bc3c88eaf046d963ae5ba9111b82
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-01 00:45:15 +0100

    Update AmazonCeo.java

--- spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/common/AmazonCeo.java
+++ spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/common/AmazonCeo.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AmazonCeo implements Ceo {
+    public AmazonCeo() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
     @Override
     public String getDailyReport() {
         return "Daily report to Jeff";
