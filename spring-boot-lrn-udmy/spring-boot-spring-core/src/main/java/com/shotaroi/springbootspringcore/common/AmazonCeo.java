b045cc8ed5acbc9f636aba12de8229bfc37000c2
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-01 12:43:44 +0100

    Update AmazonCeo.java

--- spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/common/AmazonCeo.java
+++ spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/common/AmazonCeo.java
@@ -1,15 +1,28 @@
 package com.shotaroi.springbootspringcore.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class AmazonCeo implements Ceo {
     public AmazonCeo() {
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
+
+    @PostConstruct
+    public void doWorkStuff() {
+        System.out.println("In doWorkStuff(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doCleanupStuff() {
+        System.out.println("In doCleanupStuff(): " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyReport() {
         return "Daily report to Jeff";
