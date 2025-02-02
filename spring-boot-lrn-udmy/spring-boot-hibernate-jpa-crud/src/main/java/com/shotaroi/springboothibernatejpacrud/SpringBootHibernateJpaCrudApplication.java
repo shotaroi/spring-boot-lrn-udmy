4ded87c3d8fabe7c4984fac1e1d0cf2c9aa0e00f
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-02 21:48:01 +0100

    Create SpringBootHibernateJpaCrudApplication.java

---
+++ spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/main/java/com/shotaroi/springboothibernatejpacrud/SpringBootHibernateJpaCrudApplication.java
@@ -0,0 +1,44 @@
+package com.shotaroi.springboothibernatejpacrud;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBootHibernateJpaCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHibernateJpaCrudApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+        return runner -> {
+            createStudent(studentDAO);
+            createMultipleStudents(studentDAO);
+        };
+    }
+
+    public void createMultipleStudents(StudentDAO studentDAO) {
+        System.out.println("Creating 3 student objects ... ");
+        Student tempStudent1 = new Student("John", "Doe", "John@gmail.com");
+        Student tempStudent2 = new Student("Taylor", "Swift", "Taylor@gmail.com");
+        Student tempStudent3 = new Student("Keanu", "Leevs", "Keanu@gmail.com");
+
+        System.out.println("Saving the students ...");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        System.out.println("Creating new student object ...");
+        Student tempStudent = new Student("Elon", "Musk", "elon@gmail.com");
+
+        System.out.println("Saving the student ...");
+        studentDAO.save(tempStudent);
+
+        System.out.println("Saved student. Generated id: " + tempStudent.getId());
+    }
+}
