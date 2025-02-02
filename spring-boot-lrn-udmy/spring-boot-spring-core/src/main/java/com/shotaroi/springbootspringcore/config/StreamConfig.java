677dc0dca3cff5109abf28ba391136b8d09f3626
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-02 21:31:23 +0100

    Update StreamConfig.java

--- spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/config/StreamConfig.java
+++ spring-boot-lrn-udmy/spring-boot-spring-core/src/main/java/com/shotaroi/springbootspringcore/config/StreamConfig.java
@@ -1,4 +1,13 @@
 package com.shotaroi.springbootspringcore.config;
 
+import com.shotaroi.springbootspringcore.common.Ceo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class StreamConfig {
+    @Bean("streaming")
+    public Ceo netflixCeo() {
+        return new NetflixCeo();
+    }
 }
