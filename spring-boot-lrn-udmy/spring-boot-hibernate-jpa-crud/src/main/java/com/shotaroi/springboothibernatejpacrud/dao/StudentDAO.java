1de037b333a6bd894594ad652303db1f25f5bcff
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-02 22:00:52 +0100

    Update StudentDAO.java

--- spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/main/java/com/shotaroi/springboothibernatejpacrud/dao/StudentDAO.java
+++ spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/main/java/com/shotaroi/springboothibernatejpacrud/dao/StudentDAO.java
@@ -1,5 +1,7 @@
 package com.shotaroi.springboothibernatejpacrud.dao;
 
+import com.shotaroi.springboothibernatejpacrud.entity.Student;
+
 public interface StudentDAO {
     void save(Student theStudent);
 }
