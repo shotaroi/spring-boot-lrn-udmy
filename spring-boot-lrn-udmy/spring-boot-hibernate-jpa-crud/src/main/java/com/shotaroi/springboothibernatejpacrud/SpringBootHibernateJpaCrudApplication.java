492dff8edd6a6f68f85dbfea41598b1896c0d9fe
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-02 22:00:54 +0100

    Update SpringBootHibernateJpaCrudApplication.java

--- spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/main/java/com/shotaroi/springboothibernatejpacrud/SpringBootHibernateJpaCrudApplication.java
+++ spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/main/java/com/shotaroi/springboothibernatejpacrud/SpringBootHibernateJpaCrudApplication.java
@@ -1,5 +1,7 @@
 package com.shotaroi.springboothibernatejpacrud;
 
+import com.shotaroi.springboothibernatejpacrud.dao.StudentDAO;
+import com.shotaroi.springboothibernatejpacrud.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
