89178a900dc0370414b06ec6aefb14f036cd157d
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-02 22:01:00 +0100

    Update StudentDAOImpl.java

--- spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/main/java/com/shotaroi/springboothibernatejpacrud/dao/StudentDAOImpl.java
+++ spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/main/java/com/shotaroi/springboothibernatejpacrud/dao/StudentDAOImpl.java
@@ -1,9 +1,12 @@
 package com.shotaroi.springboothibernatejpacrud.dao;
 
+import com.shotaroi.springboothibernatejpacrud.entity.Student;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class StudentDAOImpl implements StudentDAO {
     private EntityManager entityManager;
 
