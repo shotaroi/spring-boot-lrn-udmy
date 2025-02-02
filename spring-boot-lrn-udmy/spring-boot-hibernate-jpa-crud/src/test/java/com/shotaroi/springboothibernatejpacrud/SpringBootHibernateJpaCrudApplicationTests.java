5bf8105d8a68b4f715089e9c4519470ad8574280
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-02 21:47:58 +0100

    Init

---
+++ spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/test/java/com/shotaroi/springboothibernatejpacrud/SpringBootHibernateJpaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springboothibernatejpacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHibernateJpaCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
