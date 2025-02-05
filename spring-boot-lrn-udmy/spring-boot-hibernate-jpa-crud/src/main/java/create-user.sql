b3fe03a8234823520f32423373ef988983d0f496
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-06 00:47:42 +0100

    Create create-user.sql

---
+++ spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/main/java/create-user.sql
@@ -0,0 +1,6 @@
+DROP USER if exists 'springstudent'@'%' ;
+
+CREATE USER 'springstudent'@'%' IDENTIFIED BY 'springstudent';
+
+GRANT ALL PRIVILEGES ON * . * TO 'springstudent'@'%';
+                                 
