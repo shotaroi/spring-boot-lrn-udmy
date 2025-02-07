6020365b4c8cb0b369a54c46f8b51e22cbc0b20d
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-07 01:55:54 +0100

    Update SpringBootHibernateJpaCrudApplication.java

--- spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/main/java/com/shotaroi/springboothibernatejpacrud/SpringBootHibernateJpaCrudApplication.java
+++ spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/main/java/com/shotaroi/springboothibernatejpacrud/SpringBootHibernateJpaCrudApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringBootHibernateJpaCrudApplication {
 
@@ -19,8 +21,24 @@ public class SpringBootHibernateJpaCrudApplication {
         return runner -> {
 //            createStudent(studentDAO);
 //            createMultipleStudents(studentDAO);
-            readStudent(studentDAO);
+//            readStudent(studentDAO);
+//            queryForStudents(studentDAO);
+            queryForStudentsByLastName(studentDAO);
         };
+    }
+
+    private void queryForStudentsByLastName(StudentDAO studentDAO) {
+        List<Student> theStudents = studentDAO.findByLastName("Doe");
+        for (Student tempStudent : theStudents) {
+            System.out.println(tempStudent);
+        }
+    }
+
+    private void queryForStudents(StudentDAO studentDAO) {
+        List<Student> theStudents = studentDAO.findAll();
+        for (Student tempStudent : theStudents) {
+            System.out.println(tempStudent);
+        }
     }
 
     private void readStudent(StudentDAO studentDAO) {
