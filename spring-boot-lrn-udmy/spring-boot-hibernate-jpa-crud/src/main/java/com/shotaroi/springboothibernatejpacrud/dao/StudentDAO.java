7e4dd2e41110f9bbf85129e1b66c2aea3f580ff1
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-02-07 01:47:40 +0100

    Update StudentDAO.java

--- spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/main/java/com/shotaroi/springboothibernatejpacrud/dao/StudentDAO.java
+++ spring-boot-lrn-udmy/spring-boot-hibernate-jpa-crud/src/main/java/com/shotaroi/springboothibernatejpacrud/dao/StudentDAO.java
@@ -2,8 +2,13 @@ package com.shotaroi.springboothibernatejpacrud.dao;
 
 import com.shotaroi.springboothibernatejpacrud.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
     Student findById(Integer id);
+    List<Student> findAll();
+    List<Student> findByLastName(String theLastName);
+
 
 }
